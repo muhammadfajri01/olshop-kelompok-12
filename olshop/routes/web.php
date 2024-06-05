c9315815c5a8ae0eb5e241c2228ed1c3342dd269
muhammadfajri01 <sungaisail12@gmail.com>
2024-06-05 21:23:35 +0700

    add adminlte & create dashboard

--- olshop/routes/web.php
+++ olshop/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
